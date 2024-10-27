17afe0024831070aef5e9a49d7c9eb1c865f12bf
Natoune <contact@natanchiodi.fr>
2024-10-27 16:52:14 +0100

    🔊 Update logs

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ export const app = createApp({
 	onBeforeResponse: useCompressionStream,
 	onRequest: (event) => {
 		if (process.env.LOG_REQUESTS !== "true") return;
+		if (!event.path.startsWith("/color-lyrics")) return;
 		try {
 			const id = Math.random().toString(36).substr(2, 9);
 			const ip =
@@ -21,29 +22,49 @@ export const app = createApp({
 	},
 	onAfterResponse: (event, response) => {
 		if (process.env.LOG_REQUESTS !== "true") return;
+		// @ts-ignore
+		if (!event.id) return;
 		try {
 			const ip =
 				event.headers["cf-connecting-ip"] ||
 				event.headers["x-real-ip"] ||
 				event.req.socket.remoteAddress;
 
-			console.log(
-				// @ts-ignore
-				`[${event.id}] (${ip}) Response status: ${event.res.statusCode} ${event.res.statusMessage}`,
-			);
-			console.log(
-				// @ts-ignore
-				`[${event.id}] (${ip}) Response headers:`,
-				event.res.getHeaders(),
-			);
-			response?.body &&
+			if (event.res.statusCode.toString().startsWith("2")) {
 				console.log(
 					// @ts-ignore
-					`[${event.id}] (${ip}) Response body:`,
-					response.body instanceof Uint8Array
-						? `data:application/protobuf;base64,${Buffer.from(new TextDecoder().decode(response.body)).toString("base64")}`
-						: response.body.toString(),
+					`[${event.id}] (${ip}) Response status: ${event.res.statusCode} ${event.res.statusMessage}`,
 				);
+				console.log(
+					// @ts-ignore
+					`[${event.id}] (${ip}) Response headers:`,
+					event.res.getHeaders(),
+				);
+				response?.body &&
+					console.log(
+						// @ts-ignore
+						`[${event.id}] (${ip}) Response body:`,
+						response.body instanceof Uint8Array
+							? `data:application/protobuf;base64,${Buffer.from(new TextDecoder().decode(response.body)).toString("base64")}`
+							: response.body.toString(),
+					);
+			} else {
+				console.error(
+					// @ts-ignore
+					`[${event.id}] (${ip}) Response status: ${event.res.statusCode} ${event.res.statusMessage}`,
+				);
+				console.error(
+					// @ts-ignore
+					`[${event.id}] (${ip}) Response headers:`,
+					event.res.getHeaders(),
+				);
+				response?.body &&
+					console.error(
+						// @ts-ignore
+						`[${event.id}] (${ip}) Response body:`,
+						response.body.toString(),
+					);
+			}
 		} catch {}
 	},
 });
